0271f7c825e9e8640ca2a8c62b1202f77cc91fd0
alexisdiaz008 <lexmasta@gmail.com>
2017-06-23 12:06:39 -0400

    minor format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,43 @@ module ApplicationHelper
 			"Select a specific city to add to the location pool."
 		end
 	end
+
+
+	def job_template_category_options
+		[
+			['Transportation', 'transportation'], 
+			["accounting/finance", "accounting/finance"], 
+			["admin/office", "admin/office"], 
+			["architect/engineer/cad (no IT/computer jobs here please)", "architect/engineer/cad"], 
+			["art/media/design", "art/media/design"], 
+			["business/mgmt", "business/mgmt"], 
+			["customer service", "customer service"],
+			["education/teaching", "education/teaching"],
+			["et cetera", "et cetera"],
+			["food/beverage/hospitality", "food/beverage/hospitality"],
+			["general labor", "general labor"],
+			["government", "government"],
+			["healthcare", "healthcare"],
+			["human resource", "human resource"],
+			["internet engineering", "internet engineering"],
+			["legal/paralegal", "legal/paralegal"],
+			["manufacturing", "manufacturing"],
+			["marketing/advertising/pr", "marketing/advertising/pr"],
+			["nonprofit", "nonprofit"],
+			["real estate", "real estate"],
+			["retail/wholesale", "retail/wholesale"],
+			["sales", "sales"],
+			["salon/spa/fitness", "salon/spa/fitness"],
+			["science/biotech", "science/biotech"],
+			["security", "security"],
+			["skilled trades/artisan", "skilled trades/artisan"],
+			["software/qa/dba/etc", "software/qa/dba/etc"],
+			["systems/networking", "systems/networking"],
+			["technical support", "technical support"],
+			["transportation", "transportation"],
+			["tv/film/video/radio", "tv/film/video/radio"],
+			["web/html/info design", "web/html/info design"],
+			["writing/editing", "writing/editing"]
+		]
+	end
 end
